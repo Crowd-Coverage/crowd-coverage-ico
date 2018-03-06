ddd5c15f1d65371d984d069fa8d348f32b60ce46
pllearns <pllearns@gmail.com>
2018-03-05 21:18:30 -0800

    Initial commit with token contract

---
+++ contracts/COVRToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.18;
+
+import 'zeppelin-solidity/contracts/MintableToken.sol';
+
+contract COVRToken is MintableToken {
+  string public name = "Crowd Coverage Token";
+  string public symbol = "COVR";
+  uint8 public decimals = 18;
+}
