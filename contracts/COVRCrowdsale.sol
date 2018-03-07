6a3dc416b829f5c49efd3c2692acebfe9814f483
pllearns <pllearns@gmail.com>
2018-03-06 22:59:07 -0800

    More contract functions

--- contracts/COVRCrowdsale.sol
+++ contracts/COVRCrowdsale.sol
@@ -18,4 +18,49 @@ contract COVRCrowdsale is CappedCrowdSale, RefundableCrowdSale {
 
   event EthTransferred (string text);
   event EthRefunded (string text);
+
+  function COVRCrowdSale(uint _startTime, uint _endTime, uint _rate, address _wallet, uint _goal, uint _cap) CappedCrowdsale(_cap) FinalizableCrowdsale() RefundableCrowdsale(_goal) Crowdsale(_startTime, _endTime, _rate, _wallet) public {
+    require(_goal <= _cap);
+  }
+
+  function createCOVRContract() internal returns (MintableToken) {
+    return new COVRToken();
+  }
+
+  function setCrowdSaleStage(uint value) public onlyOwner {
+    CrowdsaleStage _stage;
+
+    if(uint(CrowdsaleStage.PreICO) == value) {
+      _stage = CrowdsaleStage.PreICO;
+    } else if (uint(CrowdsaleStage.ICO) == value) {
+      _stage = CrowdsaleStage.ICO;
+    }
+
+    stage = _stage;
+
+    if(stage == CrowdsaleStage.PreICO) {
+      setCurrentRate(6); // This will change before test
+    } else if (stage == CrowdsaleStage.ICO) {
+      setCurrentRate(3); // This will change before test
+    }
+
+    function setCurrentRate(uint _rate) private {
+      rate = _rate;
+    }
+  }
+
+    function () external payable {
+      uint256 tokensThatWillBeMintedAfterPurchase = msg.value.mul(rate);
+      if ((stage == CrowdsaleStage.PreICO) && (token.totalSupply() + tokensThatWillBeMintedAfterPurchase > totalTokensForSaleDuringPreICO)) {
+        msg.sender.transfer(msg.value);
+        EthRefunded("PreICO Limit Hit");
+        return;
+      }
+
+      buyTokens(msg.sender);
+
+      if (stage == CrowdsaleStage.PreICO) {
+          totalWeiRaisedDuringPreICO = totalWeiRaisedDuringPreICO.add(msg.value);
+      }
+  }
 }
